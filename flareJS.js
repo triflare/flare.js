697e89fbd0d1fb2d10085ca2e8adf49bde359bfd
DaRender <liambrynard45@gmail.com>
2026-03-30 21:06:10 -0600

    initialize (#6)

--- flareJS.js
+++ flareJS.js
@@ -1,73 +1,11 @@
+// i removed extforge
+// check for da unsandboxed
 (async function(Scratch) {
-    const variables = {};
-
-
     if (!Scratch.extensions.unsandboxed) {
-        alert("This extension needs to be unsandboxed to run!")
+        alert("Since Flare.js uses, well JS, IT needs to be unsandboxed for it to work.")
         return
     }
-
-    const ExtForge = {
-        Broadcasts: new function() {
-            this.raw_ = {};
-            this.register = (name, blocks) => {
-                this.raw_[name] = blocks;
-            };
-            this.execute = async (name) => {
-                if (this.raw_[name]) {
-                    await this.raw_[name]();
-                };
-            };
-        },
-
-        Variables: new function() {
-            this.raw_ = {};
-            this.set = (name, value) => {
-                this.raw_[name] = value;
-            };
-            this.get = (name) => {
-                return this.raw_[name] ?? null;
-            }
-        },
-
-        Vector: class {
-            constructor(x, y) {
-                this.x = x;
-                this.y = y;
-            }
-
-            static from(v) {
-                if (v instanceof ExtForge.Vector) return v
-                if (v instanceof Array) return new ExtForge.Vector(Number(v[0]), Number(v[1]))
-                if (v instanceof Object) return new ExtForge.Vector(Number(v.x), Number(v.y))
-                return new ExtForge.Vector()
-            }
-
-            add(v) {
-                return new Vector(this.x + v.x, this.y + v.y);
-            }
-
-            set(x, y) {
-                return new Vector(x ?? this.x, y ?? this.y)
-            }
-        },
-
-        Utils: {
-            setList: (list, index, value) => {
-                [...list][index] = value;
-                return list;
-            },
-            lists_foreach: {
-                index: [0],
-                value: [null],
-                depth: 0
-            },
-            countString: (x, y) => {
-                return y.length == 0 ? 0 : x.split(y).length - 1
-            }
-        }
-    }
-
+// define the extension yk
     class Extension {
         getInfo() {
             return {
@@ -85,6 +23,7 @@
                         }
                     }
                 }, {
+                    // dev console
                     "opcode": "block_c4fdf662a81dca10",
                     "text": "Developer Console Print [853ed030bb3ebc10]",
                     "blockType": "command",
@@ -95,6 +34,7 @@
                         }
                     }
                 }, {
+                    // run js
                     "opcode": "block_3448f6dc96fcdbeb",
                     "text": "JavaScript [4fe0edb270d2adbb]",
                     "blockType": "command",
@@ -118,7 +58,5 @@
         }
     }
 
-    let extension = new Extension();
-
-    Scratch.extensions.register(extension);
+    Scratch.extensions.register(new Extension());
 })(Scratch);
